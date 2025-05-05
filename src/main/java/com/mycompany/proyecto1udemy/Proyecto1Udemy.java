5e59f0147736b86d1f88587f51980519c834cf95
PaulaChiecher <paulachiecher@gmail.com>
2025-05-05 13:38:05 -0300

    Proyecto para el master de java de Udemy

---
+++ src/main/java/com/mycompany/proyecto1udemy/Proyecto1Udemy.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1udemy;
+
+/**
+ *
+ * @author paula
+ */
+import java.util.Scanner;
+public class Proyecto1Udemy {
+    static int numeroa;
+    public static void main(String[] args) {
+        System.out.println("Paula");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        numeroa = in.nextInt();
+        System.out.println("El numero es: " + numeroa);
+    }
+}
